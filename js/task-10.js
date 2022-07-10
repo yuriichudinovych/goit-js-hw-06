5feeac9b55290f9134459ed008e75a3b04c2a429
Yurii Chudinovych <yuriichudinovych93@gmail.com>
2022-07-10 17:31:54 +0300

    fix error
    refine code

--- js/task-10.js
+++ js/task-10.js
@@ -8,35 +8,34 @@ console.log(inputValue);
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 
-inputValue.addEventListener("change", () => {
-  createBoxes(Number(inputValue.value));
-});
 let containerWidth = 30;
 let containerheight = 30;
 
-function createBoxes(amount) {
-  console.log(amount);
+createBtn.addEventListener("click", () => {
+  createBoxes(Number(inputValue.value));
+});
 
-  createBtn.addEventListener("click", (event) => {
-    for (let i = 1; i <= amount; i += 1) {
-      const containerEl = document.createElement("div");
-      containerWidth += 10;
-      containerheight += 10;
-      containerEl.classList = "box-container";
-      containerEl.style.width = containerWidth + "px";
-      containerEl.style.height = containerheight + "px";
-      containerEl.style.backgroundColor = getRandomHexColor();
-      boxesEl.append(containerEl);
-    }
-    destroyBoxes();
-  });
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+  inputValue.value = "";
+});
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    const containerEl = document.createElement("div");
+    containerEl.classList = "box-container";
+    containerEl.style.width = containerWidth + "px";
+    containerEl.style.height = containerheight + "px";
+    containerWidth += 10;
+    containerheight += 10;
+    containerEl.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(containerEl);
+  }
 }
 
 function destroyBoxes() {
-  destroyBtn.addEventListener("click", () => {
-    const boxArray = [...boxesEl.children];
-    boxArray.map((el) => el.remove());
-    containerWidth = 30;
-    containerheight = 30;
-  });
+  const boxArray = [...boxesEl.children];
+  boxArray.map((el) => el.remove());
+  containerWidth = 30;
+  containerheight = 30;
 }
